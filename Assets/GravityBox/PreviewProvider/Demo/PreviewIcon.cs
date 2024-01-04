36ab11065116c17f6fca9c64c316fc5005e7b8bf
Volodymyr Dotsenko <voldemar_d@ukr.net>
2024-01-04 22:37:15 +0200

    Moved PreviewIcon to Demo folder

---
+++ Assets/GravityBox/PreviewProvider/Demo/PreviewIcon.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GravityBox.PreviewProvider;
+using UnityEngine.UI;
+
+namespace GravityBox.PreviewProvider.Demo
+{
+    public class PreviewIcon : MonoBehaviour
+    {
+        public Object renderable;
+        public Object previewGenerator;
+
+        public IPreviewProvider previewProvider => (IPreviewProvider)previewGenerator;
+
+        void Start()
+        {
+            previewProvider.RequestPreview(renderable, SetPreview);
+        }
+
+        void SetPreview(Texture preview)
+        {
+            GetComponent<RawImage>().texture = preview;
+        }
+    }
+}
